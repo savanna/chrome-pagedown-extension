e2f8dd8b775766385664567357237de6149f4080
Xiaohui Chen <savanna@gmail.com>
2018-03-24 21:44:15 -0700

    Add page up button, also add active style.

--- content.js
+++ content.js
@@ -1,12 +1,20 @@
 var container = document.createElement('div');
 container.id = 'xc-container';
+document.body.appendChild(container);
+
+var pageUpButton = document.createElement('div');
+pageUpButton.id = 'xc-pageup';
+container.appendChild(pageUpButton);
 
 var pageDownButton = document.createElement('div');
 pageDownButton.id = 'xc-pagedown';
-
 container.appendChild(pageDownButton);
-document.body.appendChild(container);
 
+
+pageUpButton.innerText = '▲';
+pageUpButton.onclick = function() {
+  window.scrollTo(0, window.scrollY - window.innerHeight * .9);
+};
 
 pageDownButton.innerText = '▼';
 pageDownButton.onclick = function() {
