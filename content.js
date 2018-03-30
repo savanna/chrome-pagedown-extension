66a6adc53ac22dc848fb7d2cb7db351cc5b5fce5
Xiaohui Chen <savanna@gmail.com>
2018-03-29 20:59:18 -0700

    ecmascript 6

--- content.js
+++ content.js
@@ -1,56 +1,63 @@
-function isTouchDevice() {
-  return 'ontouchstart' in document.documentElement;
-}
+{
+  class MainUI {
+    constructor() {
+      this.container = MainUI.createButton(
+          document.body, 'xc-container', undefined /*label*/,
+          undefined /*handler*/);
 
-function getButton(container, id, label, handler) {
-  var button = document.getElementById(id);
-  if (!button) {
-    button = document.createElement('div');
-    button.id = id;
-    if (label) {
-      button.innerText = label;
+      this.pageUpButton =
+          MainUI.createButton(this.container, 'xc-pageup', '▲', function() {
+            window.scrollTo(0, window.scrollY - window.innerHeight * .9);
+          });
+
+      this.pageDownButton =
+          MainUI.createButton(this.container, 'xc-pagedown', '▼', function() {
+            window.scrollTo(0, window.scrollY + window.innerHeight * .9);
+          });
+
+      window.onresize = this.updatePosition;
+      this.updatePosition();
     }
-    if (handler) {
-      button.onclick = handler;
+
+    static isTouchDevice() {
+      return 'ontouchstart' in document.documentElement;
     }
-    if (isTouchDevice()) {
-      // prevent long press context menu.
-      button.oncontextmenu = function(e) {
-        e.preventDefault();
-        return false;
+
+    static createButton(container, id, label, handler) {
+      let button = document.createElement('div');
+      button.id = id;
+      if (label) {
+        button.innerText = label;
+      }
+      if (handler) {
+        button.onclick = handler;
+      }
+      if (MainUI.isTouchDevice()) {
+        // prevent long press context menu.
+        button.oncontextmenu = function(e) {
+          e.preventDefault();
+          return false;
+        }
+      }
+      container.appendChild(button);
+      return button;
+    }
+
+    updatePosition() {
+      if (document.body.scrollHeight > window.innerHeight) {
+        this.container.style.display = 'block';
+        this.container.style.top =
+            Math.max(
+                0,
+                window.innerHeight - Math.max(window.innerHeight * .2, 160)) +
+            'px';
+        this.container.style.left = window.innerWidth - 96 + 'px';
+      } else {
+        // No need to scroll.
+        this.container.style.display = 'none';
       }
     }
-    container.appendChild(button);
   }
-  return button;
-};
 
-function maybeSetupButtons() {
-  var container = getButton(
-      document.body, 'xc-container', undefined /*label*/,
-      undefined /*handler*/);
-
-  var pageUpButton = getButton(container, 'xc-pageup', '▲', function() {
-    window.scrollTo(0, window.scrollY - window.innerHeight * .9);
-  });
-
-  var pageDownButton = getButton(container, 'xc-pagedown', '▼', function() {
-    window.scrollTo(0, window.scrollY + window.innerHeight * .9);
-  });
-
-  if (document.body.scrollHeight > window.innerHeight) {
-    container.style.display = 'block';
-    container.style.top =
-        Math.max(
-            0, window.innerHeight - Math.max(window.innerHeight * .2, 160)) +
-        'px';
-    container.style.left = window.innerWidth - 96 + 'px';
-  } else {
-    // No need to scroll.
-    container.style.display = 'none';
-  }
-};
-
-
-window.onresize = maybeSetupButtons;
-maybeSetupButtons();
+  var mainUI = new MainUI();
+}
